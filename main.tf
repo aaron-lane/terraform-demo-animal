f0bb2b96cddeba9c16ed9ddb0e77be43a800f3c8
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2021-04-23 15:50:40 -0400

    Use var.name as prefix

--- main.tf
+++ main.tf
@@ -11,10 +11,5 @@ resource "random_integer" "count" {
 }
 resource "random_pet" "server" {
   length = random_integer.count.result
+  prefix = var.name
 }
-
-
-#resource "aws_instance" "basic" {
-#  ami           = "ami-62131702"
-#  instance_type = "t3.nano"
-#}
